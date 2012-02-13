7a464c78cba596638fc4b190fa5bed218e5c937b
Mauro G. Santos <mintypt@gmail.com>
2012-02-13 11:23:09 +0000

    yeah. just starting.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Cli utilities
+
+My own personal cli (*command line utilities*) so I can scracth my own itch.
+
+# Utilities
+
+## nf
+
+- Creates a new file 
+- Needs improvements
+
+- **Usage:** nf.py -f file.txt
---
+++ nf.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# ln nf.py /usr/local/bin/nf.py
+# chmod a+x nf.py
+
+import os
+import optparse
+
+
+def nf():
+	p = optparse.OptionParser()
+	p.add_option('--file', '-f', default="file.txt")
+	options, arguments = p.parse_args()
+
+	file = open(options.file, 'w')
+	file.write("")
+	file.close()
+	print '> File %s created' % (options.file)
+
+if __name__ == '__main__':
+	nf()
