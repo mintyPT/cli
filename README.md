a89f138048adcaac7630dbf7a3159427545ccde4
Mauro Santos <mintypt@gmail.com>
2012-05-31 12:51:27 +0100

    New stuff

--- README.md
+++ README.md
@@ -1,51 +1,5 @@
-# Cli utilities
+# My own utilities
 
-This is the repository for my own personal cli (*command line utilities*) and custom scripts, so I can scratch my own itches because no one else will scratch them for me :)
-
-
-## Command Line Utilities
-
-### cleaName - clean names
-Small utility to help me clean the name of the files inside a directory.
-
-This utility will simply remove '.', '-', '_' and multiple
-spaces (you can edit the list of symbols at the beggining of the file). Also, it will ask permission before doing any renaming, just to be safe.
-
-**Usage:** Create a symlink and lauch inside a folder or put it inside the folder where you want to use it and launch.
-
-### nf - creates new files
-
-This is meant to create new files. I was missing the 'right-button click > new txt file' from windows. It was only after coding this, I found the 'touch' command line utility. Anyway,
-
-**Usage:** nf.py -f fileName.fileExtension
-
-### sorter.py
-
-This will:
-* Sort your files
-* Delete folders with no files
-* Delete folders with one file after extracting them to the parent folder
-
-Use carefully! 
-
-**Usage:** Create a symlink and launch inside the folder where the files to sort are, or put it inside the folder and run it.
-
-
-## Custom scripts
-
-### pyterest.py
-**I love pinups!** Once upon a time, I found a pinterest board, full of pinup images! I simply wasn't gonna download 311 images by hand so I coded this: 
-
-I present to you an image scraper from the pinterest's boards. 
-
-**Usage:** Open the file and change the url to the board and the folder to keep the images. Don't forget to create the folder before running.
-
-_ps:_ I ended up not keeping any of those pictures since in the end, it was all about writing a couple of lines in python to see if I could scrape like a mad man! 
-
-### dontEvenTrack
-Simple script to scrape the last post's title from dontevenreply.com so I can keep up. I use this script with geektool to display the info on my desktop.
-
-**Usage:** Simply run it, it prints to the console.
-
+This is the repository for my own personal cli (*command line utilities*) and custom scripts, so I can scratch my own itches because no one else will scratch  for me :)
 
 
---
+++ bitly.py
@@ -0,0 +1,136 @@
+#!/usr/bin/python
+import urllib
+import requests
+import json
+
+
+def getPageTitle(_url):
+    r = requests.get(_url)
+
+    source = r.text
+
+    _tini = source.find('<title>')
+    _tfin = source.find('</title>')
+
+    if _tini == -1 or _tfin == -1:
+        return ''
+
+    return source[_tini+7:_tfin]
+
+
+
+def bitlyAccountCreator(username, password, email):
+    
+
+    s = requests.session()
+
+    s.get('https://bitly.com/')
+
+ 
+    headers = { 'X-Requested-With': 'XMLHttpRequest',
+                'X-XSRFToken': s.cookies['_xsrf'],
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5'}
+
+    signinPayload = { 'username': username,
+                     'password': password,
+                     'email': email,
+                     'rd': '/'}
+
+
+
+    r = s.post('https://bitly.com/a/sign_up', data=signinPayload, headers=headers)
+
+    response    = json.loads(r.text)
+    status_code = response['status_code']
+    status_txt  = response['status_txt']
+
+    
+
+    if status_code == 200 and status_txt == 'OK':
+        return True
+    else:
+        for key in response['data']['errors']:
+            print '\t==> %s: \t%s' % (key, response['data']['errors'][key])
+        return False
+    
+
+
+
+def bitly(long_url, login_user, login_password):
+    
+
+    s = requests.session()
+
+    s.get('https://bitly.com/')
+
+ 
+    headers = { 'X-Requested-With': 'XMLHttpRequest',
+                'X-XSRFToken': s.cookies['_xsrf'],
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5'}
+
+    loginPayload = { 'username': 'customemoustard',
+                     'password': 'customemoustard',
+                     'rd': '/'}
+
+    longUrlPayload = {  'longUrl': long_url,
+                        'private':'false',
+                        'title': getPageTitle(long_url)}
+
+    r = s.post('https://bitly.com/a/sign_in', data=loginPayload, headers=headers)
+
+    response    = json.loads(r.text)
+    status_code = response['status_code']
+    status_txt  = response['status_txt']
+
+    boolLogin = False
+
+    if status_code == 200 and status_txt == 'OK':
+        boolLogin = True
+    else:
+        print 'something went wrong with the login'
+        print response
+        return
+    
+
+    if boolLogin == True:
+
+        r = s.post('https://bitly.com/data/beta/save', data=longUrlPayload, headers=headers)
+
+        response    = json.loads(r.text)
+        status_code = response['status_code']
+        status_txt  = response['status_txt']
+
+        if status_code == 200 and status_txt == 'OK':
+            return response['data']['link']
+
+        elif status_code == 304 and status_txt == 'LINK_ALREADY_EXISTS':
+            return response['data']['link_save']['link']
+
+        else:
+            print 'something went wrong with the url shortner'
+            print response
+            return
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    
+    login    = 'customemoustard'  
+    password = 'customemoustard'    
+
+    url = 'http://lifehacker.com/5823096/how-to-burn-your-own-lion-install-dvd-or-flash-drive'
+    
+    # print bitly(url, login, password)
+
+    username = 'manuelsilva323444'
+    email = username + '@mailinator.com'
+    password = username
+
+    # bitlyAccountCreator(username, password, email)
+
+    
+
